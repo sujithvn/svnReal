870ac87f6daa78fc5345defa1026391857968441
sujith <mail.sujithvn@gmail.com>
2020-01-06 21:16:29 +0530

    L7.7-Search

--- listings/views.py
+++ listings/views.py
@@ -20,7 +20,36 @@ def listing(request, listing_id):
 
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price and price != '1000000':
+            queryset_list = queryset_list.filter(price__lte=price)
+
     context = {
+        'listings': queryset_list,
         'price_choices': price_choices,
         'bedroom_choices': bedroom_choices,
         'state_choices': state_choices
