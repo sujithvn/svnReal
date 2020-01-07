f767de9fc2c09ffc9435ca3cc07324fcc6092d4f
sujith <mail.sujithvn@gmail.com>
2020-01-07 12:08:49 +0530

    L8.3-Accounts - Alerts/Messages shell

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def register(request):
     if request.method == 'POST':
-        return redirect('index')
+        messages.error(request, 'Testing error message')
+        return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
