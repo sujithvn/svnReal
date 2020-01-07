1442e2a72bf2edf6f72f165ba59d7578880b8adc
sujith <mail.sujithvn@gmail.com>
2020-01-07 16:15:19 +0530

    L8.6-Accounts - Logout logic and Dashboard shell

--- accounts/views.py
+++ accounts/views.py
@@ -58,7 +58,10 @@ def login(request):
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You have been logged out')
+        return redirect('index')
 
 
 def dashboard(request):
